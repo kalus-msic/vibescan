f7ef88847f4af8488358e727ee028a4c7bf9303c
Michael Kalus <michael.kalus@ms-ic.cz>
2026-04-09 01:56:37 +0200

    feat(dependencies): add homepage tabs with dependency check form and x-cloak CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["templates/**/*.html", "scanner/**/*.py"],
+  content: ["templates/**/*.html", "scanner/**/*.py", "dependencies/**/*.py"],
   theme: {
     extend: {},
   },
